d54b7643fa3c6c6a4af0eb06deb0304cba83cc52
Agnes Raimundo <agnes.raimundo.rocha@hotmail.com>
2025-09-02 02:43:58 -0300

    implementados metodos

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            
+        }
+        public override void InstalarAplicativo(string aplicativo)
+        {
+            Console.WriteLine($"instalando o aplicativo {aplicativo} no nokia");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123", "seila", "123123", 64);
+Smartphone iphone = new Iphone("456", "outro sei la", "456456", 32);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("instagram");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("face");
